fe540fd5aba7f5c5f3edc0c77c6c4373c577c71a
fjjt3 <fjjt3@yahoo.com>
2023-11-29 10:37:15 +0100

    first commit

---
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -0,0 +1,18 @@
+package com.packt.cardatabase.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository<Car,Long> {
+	// Fetch cars by brand
+	List<Car> findByBrand(String brand);
+	// Fetch cars by color
+	List<Car> findByColor(String color);
+	// Fetch cars by model year
+	List<Car> findByModelYear(int modelYear);
+	// Fetch cars by brand and model
+	List<Car> findByBrandAndModel(String brand, String model);
+	// Fetch cars by brand or color
+	List<Car> findByBrandOrColor(String brand, String color);
+}
